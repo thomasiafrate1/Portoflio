3d35ecf7742a9ac87d9cd738941d1b1865f579dc
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2025-03-24 19:19:53 +0100

    githubpages fix

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // nom EXACT du repo GitHub
+  base: '/Portfolio/Portfolio/', // nom EXACT du repo GitHub
  // Exemple : '/portfolio/'
 })
