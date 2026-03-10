41e6311f1b24a71575e03ea407d9d92c6d8304e5
Thomas Iafrate <113009615+thomasiafrate1@users.noreply.github.com>
2026-03-10 01:38:52 +0100

    fix

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portoflio/', // nom EXACT du repo GitHub
+  base: '/', // nom EXACT du repo GitHub
  // Exemple : '/portfolio/'
 })
